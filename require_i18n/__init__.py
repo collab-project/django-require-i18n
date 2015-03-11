528bedc044b6fe51ce80887f4b468d48adcf18e2
Thijs Triemstra <info@collab.nl>
2015-03-11 19:37:27 +0100

    bump version

--- require_i18n/__init__.py
+++ require_i18n/__init__.py
@@ -4,11 +4,13 @@
 from .util import extract_tower_json
 
 # version information
-__version__ = (1, 0, 0)
+__version__ = (1, 1, 0)
 
 #: For example: `2.0.0`
 short_version = '.'.join([str(x) for x in __version__[:3]])
 
 #: For example: `2.0.0a1`
-version = '{0}{1}'.format('.'.join([str(x) for x in __version__[:-1]]),
-    __version__[-1])
+if len(__version__) == 4:
+	version = '{0}{1}'.format(short_version, __version__[3])
+else:
+	version = short_version
